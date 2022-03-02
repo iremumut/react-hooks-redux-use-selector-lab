ed9ded3d757a99025c81ca620c00d0f2a4694b3e
iremumut <irem2000.nur@gmail.com>
2022-03-03 02:03:39 +0300

    show users

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {useSelector} from  "react-redux";
 // add any needed imports here
 
 function Users() {
+
+  const users = useSelector((state) => state.users);
   return (
     <div>
       <ul>
-        Users!
+      {users.length} Users! 
+        {users.map((user) => {
+          return <li key={user.username}>{user.username} - {user.hometown}</li>
+        })}
         {/* Write code here that displays the usernames of all users in the Redux store */}
         {/* In addition, display the total number of users curently in the store */}
       </ul>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import App from "./App";
-
+import { Provider } from "react-redux";
 import usersReducer from "./features/users/usersSlice";
 
+const store = createStore(usersReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 // add imports and code
 
 ReactDOM.render(
-  // add imports and code
-  <App />,
-  // add imports and code
+  <Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById("root")
 );
